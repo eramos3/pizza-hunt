6342a192f101a1adce3da373c42c9329c2cf5cce
eramos3 <cheddarshreddar3@gmail.com>
2021-02-20 20:47:56 -0800

    added reply feature

--- models/Pizza.js
+++ models/Pizza.js
@@ -29,13 +29,13 @@ const PizzaSchema = new Schema({
         toJSON: {
             virtuals: true,
             getters: true
-          },
+        },
         id: false
     }
 );
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function () {
-    return this.comments.length;
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
 });
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
